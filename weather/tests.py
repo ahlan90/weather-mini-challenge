e192727e695871310015a4c16f592d52a8c6902b
Ahlan Guarnier <ahlan90@gmail.com>
2020-08-18 21:25:31 -0300

    End of code

--- weather/tests.py
+++ weather/tests.py
@@ -12,3 +12,11 @@ class FormatStringDays(TestCase):
     def test_one_day(self):
         days = format_array_days(['Thursday'])
         self.assertEqual(days, 'Thursday.')
+
+    def test_two_days(self):
+        days = format_array_days(['Monday', 'Thursday'])
+        self.assertEqual(days, 'Monday and Thursday.')
+
+    def test_more_then_two_days(self):
+        days = format_array_days(['Monday', 'Thursday', 'Saturday'])
+        self.assertEqual(days, 'Monday, Thursday and Saturday.')
--- weather/utils.py
+++ weather/utils.py
@@ -8,20 +8,26 @@ def format_array_days(days):
 
     days_str = ""
 
-    if days:
-        if len(days) > 1:
-            days_str = days_str
-        else:
-            days_str = days[0] + "."
-    else:
-        return ""
+    if len(days) >= 3:
+        for day in days[:-2]:
+            days_str = days_str + day + ", "
+
+        days_str = days_str + days[-2] + " and " + days[-1] + "."
+
+        return days_str
+
+    if len(days) == 2:
+        return days[-2] + " and " + days[-1] + "."
+
+    if len(days) == 1:
+        return days[0] + "."
 
     return days_str
 
 
 def process_days_needs_umbrela(start_date, sum_days):
 
-    prefix = f'You should take an umbrella in these days:'
+    prefix = f'You should take an umbrella in these days: '
 
     end_date = date.today() + timedelta(days=sum_days)
 
@@ -30,7 +36,8 @@ def process_days_needs_umbrela(start_date, sum_days):
     days_string = []
 
     for day_humidity in days_humidity_interval:
-        days_string.append(calendar.day_name[day_humidity.date.weekday()])
+        if day_humidity.humidity >= 70:
+            days_string.append(calendar.day_name[day_humidity.date.weekday()])
 
     sufix = format_array_days(days_string)
 
